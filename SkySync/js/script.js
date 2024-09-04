da2fbc9505be43957c703b68fc8245ec755439a2
MMwenda <maur1cemmwenda@gmail.comgit config --global user.email maur1cemmwenda@gmail.com>
2024-09-04 11:59:49 +0300

    Button styling and 'Today's Forecast' section added

---
+++ SkySync/js/script.js
@@ -0,0 +1,4 @@
+// document.getElementById('shrinkButton').addEventListener('click', function() {
+//     this.classList.add('scale-75');
+//     setTimeout(() => this.classList.remove('scale-75'), 1000);
+//   });
